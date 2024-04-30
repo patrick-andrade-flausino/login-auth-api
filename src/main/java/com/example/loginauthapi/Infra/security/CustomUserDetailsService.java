8115b60613d59a64c72de1c06fbe27ad013ff494
patrick-andrade-flausino <patrickandradeflausino@gmail.com>
2024-04-29 22:23:39 -0300

    feat: criando aplicacao back para login com JPA, Spring secutiry

---
+++ src/main/java/com/example/loginauthapi/Infra/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.loginauthapi.Infra.security;
+
+import com.example.loginauthapi.repositories.UserRepository;
+import com.example.loginauthapi.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+    CustomUserDetailsService (UserRepository _repository){
+        this.repository = _repository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
---
+++ src/main/java/com/example/loginauthapi/Infra/security/TokenService.java
@@ -0,0 +1,49 @@
+package com.example.loginauthapi.Infra.security;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.loginauthapi.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secretKey;
+    public String generateToken(User user){
+        try{
+            Algorithm alg = Algorithm.HMAC256(secretKey);
+
+            return  JWT.create().withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExperirationDate())
+                    .sign(alg);
+        }catch(JWTCreationException ex){
+            throw new RuntimeException("Error while authenticating");
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm alg = Algorithm.HMAC256(secretKey);
+            return JWT.require(alg)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException ex){
+            return null;
+        }
+    }
+
+    private Instant generateExperirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
